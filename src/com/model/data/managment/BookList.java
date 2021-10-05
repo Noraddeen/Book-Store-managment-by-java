15996d291446c6eedfd9e243abca10427b9beac6
Noradin Hassan <90709700+Noraddeen@users.noreply.github.com>
2021-10-05 08:07:59 +0300

    Add files via upload
    this folder with the data folder for managing data

---
+++ src/com/model/data/managment/BookList.java
@@ -0,0 +1,20 @@
+package com.model.data.managment;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.model.data.Book;
+
+
+public class BookList {
+	
+	private List<Book> bookList;
+	
+	BookList(){      // using inteface for dependancy ingection
+		 
+		// this.bookList =   ;          // the type of data structure will be detrmine next week
+	}
+	                                    // function and impliments will be detrmine next week after know wich datastructue will use
+
+}
---
+++ src/com/model/data/managment/ManagerList.java
@@ -0,0 +1,16 @@
+package com.model.data.managment;
+
+import java.util.List;
+import com.model.data.Manager;
+
+public class ManagerList {
+	
+	private List<Manager> customerList; 
+	
+    ManagerList(){  // using inteface for dependancy ingection
+		
+		//this.managerList =    ;              // the type of data structure will be detrmine next week
+	}
+                                               // function and impliments will be detrmine next week after know wich datastructue will use
+
+}
