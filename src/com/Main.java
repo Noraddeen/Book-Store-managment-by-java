70aaeea45b210c99265c2fa3c815c058965083a1
RomanKarim <91786351+RomanKarim@users.noreply.github.com>
2021-11-02 08:45:28 +0300

    Add files via upload

---
+++ src/com/Main.java
@@ -0,0 +1,12 @@
+package com;
+
+import com.view.ManagerView;
+
+public class Main {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws Exception {
+        ManagerView managerView = new ManagerView();
+        managerView.getManagerViews();
+
+    }
+}
