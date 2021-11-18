73b242af3387a75ae5df51518b4394586eba56e0
RomanKarim <91786351+RomanKarim@users.noreply.github.com>
2021-11-18 23:12:40 +0300

    Update IntListFile.java

--- src/com/model/managment/IntListFile.java
+++ src/com/model/managment/IntListFile.java
@@ -2,7 +2,7 @@ package com.model.managment;
 
 import java.util.List;
 
-public interface IntListFile<Type> {
-    abstract List<Type> openAndFitch();
+public interface IntListFile<Type> {    
+    abstract List<Type> openAndFitch();      // create output stream then get list of objects from it
     abstract boolean saveAndClose(List<Type> items);
 }
