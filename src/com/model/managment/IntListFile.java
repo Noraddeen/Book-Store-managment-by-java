ee7e00ea3e2633c68eafad894a009ef6164f3833
Noradin Hassan <90709700+Noraddeen@users.noreply.github.com>
2021-11-18 22:52:11 +0300

    Delete src/com/model/managment directory

--- src/com/model/managment/IntListFile.java
+++
@@ -1,8 +0,0 @@
-package com.model.managment;
-
-import java.util.List;
-
-public interface IntListFile<Type> {
-    abstract List<Type> openAndFitch();
-    abstract boolean saveAndClose(List<Type> items);
-}
