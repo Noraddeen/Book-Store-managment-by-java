7aec578ec6a01dd7d94a882d0c6b446b6348aea2
Noradin Hassan <90709700+Noraddeen@users.noreply.github.com>
2021-11-19 21:37:20 +0300

    Update Main.java

--- src/com/Main.java
+++ src/com/Main.java
@@ -3,7 +3,7 @@ package com;
 import com.controller.ServerSideSocket;
 
 public class Main {
-    @SuppressWarnings("unchecked")
+    
     public static void main(String[] args) throws Exception {
        
       ServerSideSocket server = new ServerSideSocket();
